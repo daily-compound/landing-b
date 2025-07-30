a85bfe7030a17a15ceb5be9a88a81484f7548a6e
jaewan <ilyjw0@gmail.com>
2025-07-31 00:06:52 +0900

    everything done without copy

--- index.js
+++ index.js
@@ -1,56 +1,5 @@
 // initialization
 
-const RESPONSIVE_WIDTH = 1024
-
-let headerWhiteBg = false
-let isHeaderCollapsed = window.innerWidth < RESPONSIVE_WIDTH
-const collapseBtn = document.getElementById("collapse-btn")
-const collapseHeaderItems = document.getElementById("collapsed-header-items")
-
-
-
-function onHeaderClickOutside(e) {
-
-    if (!collapseHeaderItems.contains(e.target)) {
-        toggleHeader()
-    }
-
-}
-
-
-function toggleHeader() {
-    if (isHeaderCollapsed) {
-        // collapseHeaderItems.classList.remove("max-md:tw-opacity-0")
-        collapseHeaderItems.classList.add("opacity-100",)
-        collapseHeaderItems.style.width = "60vw"
-        collapseBtn.classList.remove("bi-list")
-        collapseBtn.classList.add("bi-x", "max-lg:tw-fixed")
-        isHeaderCollapsed = false
-
-        setTimeout(() => window.addEventListener("click", onHeaderClickOutside), 1)
-
-    } else {
-        collapseHeaderItems.classList.remove("opacity-100")
-        collapseHeaderItems.style.width = "0vw"
-        collapseBtn.classList.remove("bi-x", "max-lg:tw-fixed")
-        collapseBtn.classList.add("bi-list")
-        isHeaderCollapsed = true
-        window.removeEventListener("click", onHeaderClickOutside)
-
-    }
-}
-
-function responsive() {
-    if (window.innerWidth > RESPONSIVE_WIDTH) {
-        collapseHeaderItems.style.width = ""
-
-    } else {
-        isHeaderCollapsed = true
-    }
-}
-
-window.addEventListener("resize", responsive)
-
 
 /**
  * Animations
@@ -61,7 +10,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 gsap.to(".reveal-up", {
     opacity: 0,
-    y: "100%",
+    y: "50px",
 })
 
 // gsap.to("#dashboard", {
@@ -125,7 +74,7 @@ sections.forEach((sec) => {
     const revealUptimeline = gsap.timeline({paused: true, 
                                             scrollTrigger: {
                                                             trigger: sec,
-                                                            start: "10% 80%", // top of trigger hits the top of viewport
+                                                            start: "top 70%", // top of trigger hits the top of viewport
                                                             end: "20% 90%",
                                                             // markers: true,
                                                             // scrub: 1,
